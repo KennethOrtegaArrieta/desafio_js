155ba5d82739557a3ce6ddca68da56158e7d7853
Kenneth Ortega <kennethortegarrieta@gmail.com>
2024-08-17 19:10:45 -0600

    Add files via upload

---
+++ desafio 7 días/diauno.js
@@ -0,0 +1,21 @@
+let numeroUn=1;
+let stringUn='1';
+let numeroTreinta=30;
+let stringTreinta='30';
+let numeroDiez=10;
+let stringDiez='10';
+if( numeroUn==stringUn){
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero diferentes')
+}else{
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+}
+if(numeroTreinta===stringTreinta){
+    console.log('Las varibles numeroTreinta y stringTreinta  tiene el mismo valor y el mismo tipo')
+}else{
+    console.log('Las variables numeroTreinta y stringTreinta no tiene el mismo tipo')
+}
+if(numeroDiez==stringDiez){
+console.log('Las varibles numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+}else{
+console.log('Las variables numeroDiez y stringDiez no tiene el mismo valor')
+}
